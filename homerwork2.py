6f4dc975c452a17cbf93969a2a33c5beac36eeff
Mika Hertler <mikahertler@gmail.com>
2021-04-09 00:27:05 +0200

    Finish Homework#2

---
+++ homerwork2.py
@@ -0,0 +1,16 @@
+def Genre():
+    print ("Metal")
+
+def Artist():
+    print ("Rammstein")
+
+def Releaseyear():
+    print ("2001")
+
+def is_Rammstein_dead():
+    print(bool(False))
+
+Genre()
+Artist()
+Releaseyear()
+is_Rammstein_dead()
