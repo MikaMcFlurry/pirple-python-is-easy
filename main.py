c34bb791e17643d886bf62f79b9344b0baf8570f
Mika Hertler <mikahertler@gmail.com>
2021-04-08 20:46:00 +0200

    Finish Homework #1

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+Here are some facts about the song Sonne by Rammstein
+"""
+
+Song = "Sonne"
+Artist = "Rammstein"
+Genre = "Metal"
+Album = "Mutter"
+Releas = 2001
+Duration = 4.53
+Spotifystreams = 150118217
+
+print (Song)
+print (Artist)
+print (Genre)
+print (Album)
+print (Releas)
+print (Duration)
+print (Spotifystreams)
