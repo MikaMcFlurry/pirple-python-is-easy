c045b7a9dfebded74ba503352d58ce7169fef298
Mika Hertler <mikahertler@gmail.com>
2021-04-11 18:38:41 +0200

    Finish Homework#5

---
+++ homework5.py
@@ -0,0 +1,56 @@
+prime_counter = 0
+MAX_NUMBER = 100
+
+def FizzBuzz_while():
+    number = 1
+    while number <= MAX_NUMBER:
+        if is_prime(number) == True:
+            print("prime")
+
+        elif number % 3 == 0 and number % 5 != 0:
+            print("Fizz")
+
+        elif number % 5 == 0 and number % 3 != 0:
+            print("Buzz")
+
+        elif number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+
+        else:
+            print(number)
+
+        number += 1
+
+def FizzBuzz_for():
+    for i in range(1, MAX_NUMBER + 1):
+        if is_prime(i) == True:
+            print("prime")
+        
+        elif i % 3 == 0 and i % 5 != 0:
+            print("Fizz")
+
+        elif i % 5 == 0 and i % 3 != 0:
+            print("Buzz")
+
+        elif i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+
+        else:
+            print(i)
+
+def is_prime(number):
+    global prime_counter
+
+    if number < 2:
+        return False
+
+    for i in range(2, number):    
+        if number % i == 0:
+            return False
+
+    prime_counter += 1
+    return True 
+        
+
+FizzBuzz_for()
+print(f"There are {prime_counter} primes below {MAX_NUMBER}!!!")
