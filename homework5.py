262dd5f4205afe6340a3f437d72eddcee4df44dd
Mika Hertler <mikahertler@gmail.com>
2021-04-11 23:07:48 +0200

    Change the algorithm to check for primes, for more efficiency

--- homework5.py
+++ homework5.py
@@ -1,11 +1,12 @@
-prime_counter = 0
-MAX_NUMBER = 100
+import math
+MAX_NUMBER = 100000
+
 
 def FizzBuzz_while():
     number = 1
     while number <= MAX_NUMBER:
-        if is_prime(number) == True:
-            print("prime")
+        if is_prime(number):
+            print("Prime")
 
         elif number % 3 == 0 and number % 5 != 0:
             print("Fizz")
@@ -21,11 +22,12 @@ def FizzBuzz_while():
 
         number += 1
 
+
 def FizzBuzz_for():
     for i in range(1, MAX_NUMBER + 1):
-        if is_prime(i) == True:
+        if is_prime(i):
             print("prime")
-        
+
         elif i % 3 == 0 and i % 5 != 0:
             print("Fizz")
 
@@ -38,19 +40,36 @@ def FizzBuzz_for():
         else:
             print(i)
 
-def is_prime(number):
-    global prime_counter
 
-    if number < 2:
+def is_prime(number):
+    """
+    Checks if a number is a prime.
+    """
+    if number == 2:
+        return True
+
+    if number < 2 or number % 2 == 0:
         return False
 
-    for i in range(2, number):    
+    return check_prime(number)
+
+def check_prime(number):
+    """
+    Checks if an odd number greater than 1 is a prime.
+    """
+    for i in range(3, math.ceil(math.sqrt(number)) + 1, 2):
         if number % i == 0:
             return False
 
-    prime_counter += 1
-    return True 
-        
+    return True
 
-FizzBuzz_for()
-print(f"There are {prime_counter} primes below {MAX_NUMBER}!!!")
+def count_primes():
+    prime_counter = 1
+    for i in range(3, MAX_NUMBER + 1, 2):
+        if check_prime(i):
+            prime_counter += 1
+    return prime_counter
+
+
+#FizzBuzz_for()
+print(f"There are {count_primes()} primes below {MAX_NUMBER}!!!")
