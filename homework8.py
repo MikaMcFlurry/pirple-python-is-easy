b534f28a1d89356d88474e17c463ecb35722780b
Mika Hertler <mikahertler@gmail.com>
2021-07-11 23:24:31 +0200

    Add extra Credit (4th option)

--- homework8.py
+++ homework8.py
@@ -2,7 +2,7 @@ import os.path
 
 filename = input("Type in your filename: \n")
 if os.path.isfile(filename) == True:
-    action =  input("Do you want to \n A) Read the file (type A) \n B) Delete the file and start over (type B) \n C) Append the file (type C) \n D) Append the file (type D)\n") 
+    action =  input("Do you want to \n A) Read the file (type A) \n B) Delete the file and start over (type B) \n C) Append the file (type C) \n D) Change a specific line (type D)\n") 
     if action == "A":
         file = open(filename, "r")
         print(file.read())
@@ -13,13 +13,15 @@ if os.path.isfile(filename) == True:
         file.close
     elif action == "C":
         file = open(filename, "a")
+        file.write("\n")
         file.write(input("Type in your note here:\n"))
         file.close
     elif action == "D":
         file = open(filename, "r")
         list_of_lines = file.readlines()
-        line = input("Wich line do you want to change?\n")
-        list_of_lines[line] = input("Type in your replacement here:\n")
+        line = int(input("Wich line do you want to change?\n"))
+        tempTxt = input("Type in your replacement here:\n")
+        list_of_lines[line - 1] = tempTxt + "\n"
         file.close
 
         file = open(filename, "w")
@@ -31,7 +33,3 @@ else:
     file = open(filename, "w")
     file.write(input("Type in your note here:\n"))
     file.close
-
-        
-
-
