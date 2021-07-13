de96ec5f9806f760d0dea9d47dccf846cd26eaaa
Mika Hertler <mikahertler@gmail.com>
2021-07-13 02:04:56 +0200

    Fix all bugs Finish the project with all needed functions

--- connect-4.py
+++ connect-4.py
@@ -5,12 +5,20 @@ row_4 = ["/  ","/  ","/  ","/  ","/  ","/  ","/  ","/"]
 row_5 = ["/  ","/  ","/  ","/  ","/  ","/  ","/  ","/"]
 row_6 = ["/  ","/  ","/  ","/  ","/  ","/  ","/  ","/"]
 column_number = [" 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 "]
-row_counter = 6
 round_counter = 1
 
 def get_column():
-    temp_column = int(input("Choose a colum:\n"))
+    global temp_column
+    global row_counter
+    global round_counter
+    ask_for_column = "Player 1 choose a colum:\n"
+    if round_counter % 2 != 0:
+           ask_for_column = "Player 1 choose a colum:\n"
+    else:
+        ask_for_column = "Player 2 choose a colum:\n"
+    temp_column = int(input(ask_for_column))
     temp_column = temp_column - 1
+    row_counter = 6
     creat_new_field()
 
 def draw_field():
@@ -25,9 +33,11 @@ def draw_field():
     get_column()
 
 def draw_new_field():
+    global round_counter
     if row_counter == 6:
         if round_counter % 2 == 0:
             row_6[temp_column] = "/X "
+            print("Player 1 = O\nPlayer 2 = X")
             print("".join(row_1))
             print("".join(row_2))
             print("".join(row_3))
@@ -37,6 +47,7 @@ def draw_new_field():
             print("".join(column_number))
         else:
             row_6[temp_column] = "/O "
+            print("Player 1 = O\nPlayer 2 = X")
             print("".join(row_1))
             print("".join(row_2))
             print("".join(row_3))
@@ -46,7 +57,8 @@ def draw_new_field():
             print("".join(column_number))
     elif row_counter == 5:
         if round_counter % 2 == 0:
-            row_5[temp_column] = "/X"
+            row_5[temp_column] = "/X "
+            print("Player 1 = O\nPlayer 2 = X")
             print("".join(row_1))
             print("".join(row_2))
             print("".join(row_3))
@@ -56,6 +68,7 @@ def draw_new_field():
             print("".join(column_number))
         else:
             row_5[temp_column] = "/O "
+            print("Player 1 = O\nPlayer 2 = X")
             print("".join(row_1))
             print("".join(row_2))
             print("".join(row_3))
@@ -66,6 +79,7 @@ def draw_new_field():
     elif row_counter == 4:
         if round_counter % 2 == 0:
             row_4[temp_column] = "/X "
+            print("Player 1 = O\nPlayer 2 = X")
             print("".join(row_1))
             print("".join(row_2))
             print("".join(row_3))
@@ -75,6 +89,7 @@ def draw_new_field():
             print("".join(column_number))
         else:
             row_4[temp_column] = "/O "
+            print("Player 1 = O\nPlayer 2 = X")
             print("".join(row_1))
             print("".join(row_2))
             print("".join(row_3))
@@ -85,6 +100,7 @@ def draw_new_field():
     elif row_counter == 3:
         if round_counter % 2 == 0:
             row_3[temp_column] = "/X "
+            print("Player 1 = O\nPlayer 2 = X")
             print("".join(row_1))
             print("".join(row_2))
             print("".join(row_3))
@@ -94,6 +110,7 @@ def draw_new_field():
             print("".join(column_number))
         else:
             row_3[temp_column] = "/O "
+            print("Player 1 = O\nPlayer 2 = X")
             print("".join(row_1))
             print("".join(row_2))
             print("".join(row_3))
@@ -104,6 +121,7 @@ def draw_new_field():
     elif row_counter == 2:
         if round_counter % 2 == 0:
             row_2[temp_column] = "/X "
+            print("Player 1 = O\nPlayer 2 = X")
             print("".join(row_1))
             print("".join(row_2))
             print("".join(row_3))
@@ -113,6 +131,7 @@ def draw_new_field():
             print("".join(column_number))
         else:
             row_2[temp_column] = "/O "
+            print("Player 1 = O\nPlayer 2 = X")
             print("".join(row_1))
             print("".join(row_2))
             print("".join(row_3))
@@ -123,6 +142,7 @@ def draw_new_field():
     elif row_counter == 1:
         if round_counter % 2 == 0:
             row_1[temp_column] = "/X "
+            print("Player 1 = O\nPlayer 2 = X")
             print("".join(row_1))
             print("".join(row_2))
             print("".join(row_3))
@@ -132,6 +152,7 @@ def draw_new_field():
             print("".join(column_number))
         else:
             row_1[temp_column] = "/O "
+            print("Player 1 = O\nPlayer 2 = X")
             print("".join(row_1))
             print("".join(row_2))
             print("".join(row_3))
@@ -143,6 +164,7 @@ def draw_new_field():
     check_if_winner()
 
 def creat_new_field():
+    global row_counter
     if "/O " in row_6[temp_column] or "/X " in row_6[temp_column]:
         row_counter = row_counter - 1
         if "/O " in row_5[temp_column] or "/X " in row_5[temp_column]:
